df534a55babd855a3a618b084dd1591cef44d28b
Johann Schopplich <mail@johannschopplich.com>
2022-12-19 08:23:37 +0100

    chore: add prepare script for types

---
+++ scripts/prepare.mjs
@@ -0,0 +1,11 @@
+import { createNitro, writeTypes } from 'nitropack'
+
+export async function prepare({ rootDir = process.cwd() } = {}) {
+  const nitro = await createNitro({
+    rootDir,
+  })
+
+  await writeTypes(nitro)
+}
+
+prepare().catch(console.error)
