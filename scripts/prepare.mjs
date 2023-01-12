bd92521782a34c511ad0a867c6fc63396144b87a
Johann Schopplich <mail@johannschopplich.com>
2023-01-12 16:07:47 +0100

    chore: update VSCode settings

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -2,10 +2,7 @@
 import { createNitro, writeTypes } from 'nitropack'
 
 export async function prepare({ rootDir = process.cwd() } = {}) {
-  const nitro = await createNitro({
-    rootDir,
-  })
-
+  const nitro = await createNitro({ rootDir })
   await writeTypes(nitro)
 }
 
