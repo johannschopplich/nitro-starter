557ca512545014b77c6cd9808b69dbb80136f2ca
Johann Schopplich <mail@johannschopplich.com>
2023-02-14 13:31:46 +0100

    chore: use new built-in prepare command

--- scripts/prepare.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { createNitro, writeTypes } from 'nitropack'
-
-export async function prepare({ rootDir = process.cwd() } = {}) {
-  const nitro = await createNitro({ rootDir })
-  await writeTypes(nitro)
-}
-
-prepare().catch(console.error)
