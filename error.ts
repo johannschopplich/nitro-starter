498abaf91f37b9c33888e793e052d3bec4a998ec
Johann Schopplich <mail@johannschopplich.com>
2023-06-19 21:36:28 +0200

    refactor: re-use error normalizer from nitro

--- error.ts
+++ error.ts
@@ -1,7 +1,7 @@
 // Custom error handler for production environment
 // See: https://github.com/unjs/nitro/blob/20078be2430bc4615119779a07f4b73e7323df60/src/runtime/error.ts
 import { setResponseHeader, setResponseStatus } from 'h3'
-import type { H3Error } from 'h3'
+import { normalizeError } from '#internal/nitro/utils'
 import type { NitroErrorHandler } from 'nitropack'
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -9,6 +9,23 @@ const isDev = process.env.NODE_ENV === 'development'
 const errorHandler: NitroErrorHandler = function (error, event) {
   const { stack, statusCode, statusMessage, message } = normalizeError(error)
   const url = event.node.req.url || ''
+
+  // Console output
+  if (error.unhandled || error.fatal) {
+    const tags = [
+      '[nitro]',
+      '[request error]',
+      error.unhandled && '[unhandled]',
+      error.fatal && '[fatal]',
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    console.error(
+      tags,
+      error.message + '\n' + stack.map((l) => '  ' + l.text).join('  \n')
+    )
+  }
 
   setResponseStatus(event, statusCode, statusMessage)
   setResponseHeader(event, 'Content-Type', 'application/json')
@@ -25,37 +42,3 @@ const errorHandler: NitroErrorHandler = function (error, event) {
 }
 
 export default errorHandler
-
-function normalizeError(error: H3Error) {
-  const cwd = typeof process.cwd === 'function' ? process.cwd() : '/'
-  const stack = (error.stack || '')
-    .split('\n')
-    .splice(1)
-    .filter((line) => line.includes('at '))
-    .map((line) => {
-      const text = line
-        .replace(cwd + '/', './')
-        .replace('webpack:/', '')
-        .replace('file://', '')
-        .trim()
-      return {
-        text,
-        internal:
-          (line.includes('node_modules') && !line.includes('.cache')) ||
-          line.includes('internal') ||
-          line.includes('new Promise'),
-      }
-    })
-
-  const statusCode = error.statusCode || 500
-  const statusMessage =
-    error.statusMessage ?? (statusCode === 404 ? 'Not Found' : '')
-  const message = error.message || error.toString()
-
-  return {
-    stack,
-    statusCode,
-    statusMessage,
-    message,
-  }
-}
