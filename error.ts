fac5729d5ef6e51d15ce6223733f1e3f4dd6ee91
Johann Schopplich <mail@johannschopplich.com>
2024-03-13 13:38:59 +0100

    chore: upgrade deps & error handler

--- error.ts
+++ error.ts
@@ -1,12 +1,11 @@
 // Custom error handler for production environment
 // See: https://github.com/unjs/nitro/blob/20078be2430bc4615119779a07f4b73e7323df60/src/runtime/error.ts
-import { setResponseHeader, setResponseStatus } from 'h3'
 import type { NitroErrorHandler } from 'nitropack'
 import { normalizeError } from '#internal/nitro/utils'
 
 const isDev = process.env.NODE_ENV === 'development'
 
-const errorHandler: NitroErrorHandler = function (error, event) {
+export default defineNitroErrorHandler(((error, event) => {
   const { stack, statusCode, statusMessage, message } = normalizeError(error)
   const url = event.path
 
@@ -40,6 +39,4 @@ const errorHandler: NitroErrorHandler = function (error, event) {
       stack: isDev && statusCode !== 404 ? stack.map((i) => i.text) : undefined,
     }),
   )
-}
-
-export default errorHandler
+}) satisfies NitroErrorHandler)
