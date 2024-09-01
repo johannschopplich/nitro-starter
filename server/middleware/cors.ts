c65d8badf2a4ae2b87871934db2571571a930387
Johann Schopplich <mail@johannschopplich.com>
2024-09-01 12:15:02 +0200

    fix: cors route

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -2,15 +2,14 @@ export default defineEventHandler((event) => {
   const {
     cors: { origins = [] },
   } = useRuntimeConfig()
-
-  // Allow requests from the development server
-  if (import.meta.dev) {
-    const origin = getRequestHeader(event, 'origin')
-    origins.push(origin)
-  }
+  const origin = getRequestHeader(event, 'origin')
 
   handleCors(event, {
-    origin: origins,
+    origin: [
+      ...origins,
+      // Allow requests from the development server
+      ...(import.meta.dev ? [origin] : []),
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
     maxAge: '86400',
   })
